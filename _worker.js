53ff757a625844ce60edf9de11d712d5bd4fa098
xuqian-liang <ae861@126.com>
2023-07-09 23:27:04 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,10 +1,13 @@
-const { createServer } = require("http");
-const { createProxyServer } = require("http-proxy");
+const express = require("express");
+const httpProxy = require("http-proxy");
 
-const proxy = createProxyServer({});
+const app = express();
+const proxy = httpProxy.createProxyServer();
 
-createServer((req, res) => {
+app.all("*", (req, res) => {
   proxy.web(req, res, { target: "https://feedback.aliexpress.com" });
-}).listen(process.env.PORT || 3000, () => {
-  console.log("反向代理服务器已启动");
+});
+
+app.listen(3000, () => {
+  console.log("反向代理服务器已启动在端口 3000");
 });
